3ea27942025515ea9b479212abd029ea2fff1ac7
ianmaboimonene <ian.maboi@student.moringaschool.com>
2023-09-21 21:09:00 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let greeting;
+  if (someValue <=400) {
+    greeting ='This one is on me!';
+  }else if (someValue >2000 && someValue <=2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  }else if (someValue >2500) {
+    greeting ='No can do.';
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  const response =city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let charm;
+  switch (tip){
+    case 'generous':
+    charm ='Thank you so much.';
+    break;
+    case 'not as generous':
+    charm = 'Thank you.';
+    break;
+    default:
+      charm ='Bye.';
+  }
+  return charm;
 }
